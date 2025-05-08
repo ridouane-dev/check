901d6424a18580aefc4068f3b4fd9758b3746c4c
ridouane-dev <ridouaneibrahim46@gmail.com>
2025-05-08 18:56:59 +0200

    kjhfejnvj

---
+++ check_file_exist.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#commande pour demander le nom du fichier 
+
+read -p "Entrez le nom du fichier :" filename
+
+#condition pour verifier si le fichier eexiste
+#si le fichier filename existe alors i affichera le message suivans
+if [ -f $filename ]; then
+
+  echo "Le fichier '$filename' existe"
+
+#dans le cas contriare il affichera le message suivants
+else
+  echo "Le fichier '$filename' n'existe pas "
+fi
